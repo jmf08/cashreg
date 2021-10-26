c46b933b783770f3d7e1fccd824784b6be65f1b8
RichardManipon <91706171+RichardManipon@users.noreply.github.com>
2021-10-27 00:02:22 +0800

    Update settings.py

--- elective2/sample/settings.py
+++ elective2/sample/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/Media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
