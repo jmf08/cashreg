741932543c264ce54c3a5f2a9c7504f881f85322
RichardManipon <91706171+RichardManipon@users.noreply.github.com>
2021-10-27 00:01:15 +0800

    Update urls.py

--- elective2/sampleApp/urls.py
+++ elective2/sampleApp/urls.py
@@ -6,9 +6,19 @@ from . import views
 urlpatterns = [
    path('', views.login, name='login'),
    path('Inventory/', views.inventory, name='inventory'),
-   path('AddInventory/',views.Add_inventory, name='AddInventory'),
+   path('Inventory/Inventory', views.inventory, name='inventory'),
+   path('Inventory/CashRegister', views.Cash_Register, name='CashRegister'),
+   path('Inventory/CashRegIframe', views.Cash_Regiframe, name='CashRegIframe'),
+   path('Inventory/Add Inventory',views.Add_inventory, name='AddInventory'),
    path('CashRegister/', views.Cash_Register, name='CashRegister'),
+   path('CashRegister/CashRegIframe/',views.Cash_Regiframe, name='CashRegIframe'),
    path('CashRegIframe/',views.Cash_Regiframe, name='CashRegIframe'),
+   path('Inventory/login', views.login, name='login'),
    path('Signup/',views.signup, name= 'Signup'),
+   path('Signup/login',views.login, name= 'login'),
+   path('login', views.login, name='login'),
+   path('CashRegister/login',views.login, name= 'login'),
+   path('CashRegister/Inventory',views.inventory, name= 'Inventory'),
+   path('CashRegister/CashRegister', views.Cash_Register, name='CashRegister'),
+   path('CashRegister/Add Inventory',views.Add_inventory, name='AddInventory'),
 ]
-
