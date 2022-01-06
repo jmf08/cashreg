a4d8877e22b7843d373588cf2fc757c055a0947d
daryl02141 <91712242+daryl02141@users.noreply.github.com>
2022-01-06 20:36:42 +0800

    Update urls.py

--- elective2/sampleApp/urls.py
+++ elective2/sampleApp/urls.py
@@ -4,14 +4,16 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-   path('', views.login, name='login'),
+   path('', views.Cash_Register, name='CashRegister'),
+   path('login/', views.Login, name='Login'),
    path('Inventory/', views.inventory, name='inventory'),
-   path('CashRegister/', views.Cash_Register, name='CashRegister'),
-   path('CashRegister/CashRegIframe/',views.Cash_Regiframe, name='CashRegIframe'),
+ 
+   path('CashRegIframe/',views.Cash_Regiframe, name='CashRegIframe'),
    path('Signup/',views.signup, name= 'Signup'),
    path('Add Inventory/',views.Add_inventory, name='AddInventory'),
    path('update/<update_id>/',views.updateitems, name='update'),
    path('delete/<update_id>/',views.deleteitems, name='delete'),
    path('delete2/<update_id2>/',views.deleteitems2, name='delete2'),
    path('delete3/<update_id3>/',views.deleteitems3, name='delete3'),
+   path('logout/',views.logoutUser, name= 'logout'),
 ]
