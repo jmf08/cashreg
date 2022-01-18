4dee8b7e52a5a649b8f046c31ac25a2b8de19550
James Ferrer <45894200+jmf08@users.noreply.github.com>
2022-01-18 23:23:53 +0800

    Add files via upload

---
+++ sampleApp/form.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django.db import models
+from django.forms import ModelForm
+from .models import cashreg1, cashreg2, cashreg3,cashreg4,cashreg5,login
+
+class cr(ModelForm):
+    class Meta:
+        model = cashreg1
+        fields = ['ItemName', 'ProductCode','Stocks','Price']
+
+class cr2(ModelForm):
+    class Meta:
+        model = cashreg2
+        fields = ['ProductCode2','ItemName2','Quantity2','Price2']
+
+class cr3(ModelForm):
+    class Meta:
+        model = cashreg3
+        fields = ['Total3', 'Cash3','Change3']
+
+class cr4(ModelForm):
+    class Meta:
+        model = cashreg4
+        fields = ['ProductCode2','ItemName2','Quantity2','Price2']
+
+class cr5(ModelForm):
+    class Meta:
+        model = cashreg5
+        fields = ['Total3', 'Cash3','Change3']
+
+class CreateUser(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','password1','password2']
